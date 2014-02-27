dc7f5ed6e1c54ad14e3e6cfef190413b81a88d50
sambuddhabasu <sambuddhabasu@ubuntu.(none)>
2014-02-28 03:56:35 +0530

    IRC bot

---
+++ ircbot.py
@@ -0,0 +1,37 @@
+"An IRC bot that responds to the users"
+"Made By Sambuddha Basu"
+import socket
+SERVER = 'irc.freenode.net'
+PORT = 6667
+CHANNEL = '#bookie'
+BOTNICK = 'askbot'
+def ping():
+    irc.send("PONG :pingis\n")
+def sendmsg(msg, usernick=None):
+    if usernick:
+        irc.send("PRIVMSG " + CHANNEL + " :" + msg + " " + usernick + "\n")
+    else:
+	irc.send("PRIVMSG " + CHANNEL + " :" + msg + "\n")
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((SERVER, PORT))
+irc.send("USER " + BOTNICK + " " + BOTNICK + " " + BOTNICK + " :I am a good bot\n")
+irc.send("NICK " + BOTNICK + "\n")
+irc.send("JOIN " + CHANNEL + "\n")
+while 1:
+    msg = irc.recv(2048)
+    msg = msg.strip('\n\r')
+    print msg
+    if msg.lower().find(":hello " + BOTNICK) != -1:
+	usernick = msg[1:msg.find("!uid")]
+        sendmsg("Hello", usernick)
+    if msg.lower().find(":hi " + BOTNICK) != -1:
+	usernick = msg[1:msg.find("!uid")]
+	sendmsg("Hi", usernick)
+    if msg.lower().find(":creator " + BOTNICK) != -1:
+	sendmsg("I am an IRC bot. My creator is Sambuddha Basu samgtr")
+    if msg.lower().find(":site " + BOTNICK) != -1:
+	sendmsg("Our website is at https://bmark.us/")
+    if msg.lower().find(":repo " + BOTNICK) != -1:
+	sendmsg("The central bookie repo is at https://github.com/bookieio/Bookie")
+    if msg.find("PING :") != -1:
+        ping()
